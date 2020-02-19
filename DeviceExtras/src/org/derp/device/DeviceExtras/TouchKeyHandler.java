50827e18f0db20fdc5bdc464406e5eb650f30a54
Anushek Prasal <anushekprasal@gmail.com>
2020-02-20 02:07:24 +0530

    hardware: DeviceExtras: TouchGestures: Add wake device action
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- DeviceExtras/src/org/derp/device/DeviceExtras/TouchKeyHandler.java
+++ DeviceExtras/src/org/derp/device/DeviceExtras/TouchKeyHandler.java
@@ -232,6 +232,9 @@ public class TouchKeyHandler implements DeviceKeyHandler {
                 case Constants.ACTION_AMBIENT_DISPLAY:
                     launchDozePulse();
                     break;
+                case Constants.ACTION_WAKE_DEVICE:
+                    wakeDevice();
+                    break;
             }
         }
     }
@@ -328,6 +331,11 @@ public class TouchKeyHandler implements DeviceKeyHandler {
             final Intent intent = new Intent(PULSE_ACTION);
             mContext.sendBroadcastAsUser(intent, UserHandle.CURRENT);           
         }
+    }
+
+    private void wakeDevice() {
+        mGestureWakeLock.acquire(GESTURE_WAKELOCK_DURATION);
+        mPowerManager.wakeUp(SystemClock.uptimeMillis(), GESTURE_WAKEUP_REASON);
     }
 
     private void dispatchMediaKeyWithWakeLockToMediaSession(final int keycode) {
