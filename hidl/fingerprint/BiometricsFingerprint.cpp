da170bf14425ff74df371669aa21fad9853f0cfc
alk3pInjection <webmaster@raspii.tech>
2021-11-25 15:04:03 +0530

    hw/op: fingerprint: Overwrite power status to bypass dimming killer
    * Fix the HBM flicker when registering fp for the first time.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -45,6 +45,7 @@
 #define NATIVE_DISPLAY_P3 "/sys/class/drm/card0-DSI-1/native_display_p3_mode"
 #define NATIVE_DISPLAY_SRGB "/sys/class/drm/card0-DSI-1/native_display_srgb_color_mode"
 #define NATIVE_DISPLAY_WIDE "/sys/class/drm/card0-DSI-1/native_display_wide_color_mode"
+#define POWER_STATUS_PATH "/sys/class/drm/card0-DSI-1/power_status"
 
 
 int c_p3,c_srgb,p3,srgb,wide;
@@ -252,6 +253,7 @@ Return<uint64_t> BiometricsFingerprint::setNotify(
 }
 
 Return<uint64_t> BiometricsFingerprint::preEnroll()  {
+    set(POWER_STATUS_PATH, 1);
     return mDevice->pre_enroll(mDevice);
 }
 
@@ -310,6 +312,7 @@ Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid,
 
 Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId,
         uint32_t gid) {
+    set(POWER_STATUS_PATH, 1);
     mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
 
