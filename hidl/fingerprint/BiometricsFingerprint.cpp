a35309e3a750334358affd9bbf09898e4224f4b3
alk3pInjection <webmaster@raspii.tech>
2022-01-22 12:34:45 +0800

    hidl: fingerprint: General improvements
    * Effectively optimize the following scenarios:
    
    - The flicker upon turning off the panel is gone.
    
    - Set "lock after screen timeout" to a higher value,
      then let the screen turn off by timeout and wake it up.
      The dimlayer will not appear.
    
    Reported-by: Pranav <npv12@iitbbs.ac.in>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: Ia7bba1d2e58bf46d66b3a424a1fc233299acc98b

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -46,7 +46,10 @@
 #define NATIVE_DISPLAY_SRGB "/sys/class/drm/card0-DSI-1/native_display_srgb_color_mode"
 #define NATIVE_DISPLAY_WIDE "/sys/class/drm/card0-DSI-1/native_display_wide_color_mode"
 #define POWER_STATUS_PATH "/sys/class/drm/card0-DSI-1/power_status"
+#define AUTH_STATUS_PATH   "/sys/class/drm/card0-DSI-1/auth_status"
+#define CANCEL_STATUS_PATH "/sys/class/drm/card0-DSI-1/cancel_status"
 
+int isCancelled = 0;
 
 int c_p3,c_srgb,p3,srgb,wide;
 bool dcDimState;
@@ -114,13 +117,13 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
 }
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
-    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
     mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
 
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
+    isCancelled = 0;
     mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
     return Void();
@@ -281,9 +284,11 @@ Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
 }
 
 Return<RequestStatus> BiometricsFingerprint::cancel() {
+    isCancelled = 1;
     mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     mVendorFpService->updateStatus(OP_FINISH_FP_ENROLL);
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
+    set(CANCEL_STATUS_PATH, 1);
 
     return ErrorFilter(mDevice->cancel(mDevice));
 }
@@ -313,7 +318,11 @@ Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid,
 Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId,
         uint32_t gid) {
     set(POWER_STATUS_PATH, 1);
-    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    set(CANCEL_STATUS_PATH, 0);
+    if (isCancelled)
+        mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    else
+        set(AUTH_STATUS_PATH, 1);
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
 
     return ErrorFilter(mDevice->authenticate(mDevice, operationId, gid));
