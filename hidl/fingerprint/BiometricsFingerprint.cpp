7e65ca7ea52772b9db23d33863b76524b081888f
Pranav <npv12@iitbbs.ac.in>
2022-02-17 08:33:42 +0530

    Revert "hidl: fingerprint: General improvements"
    This reverts commit b361fb0b03d62d6baf50a1f1d9b9af8203b4adea.

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -34,10 +34,7 @@
 #define OP_DISPLAY_SET_DIM 10
 
 #define POWER_STATUS_PATH "/sys/class/drm/card0-DSI-1/power_status"
-#define AUTH_STATUS_PATH   "/sys/class/drm/card0-DSI-1/auth_status"
-#define CANCEL_STATUS_PATH "/sys/class/drm/card0-DSI-1/cancel_status"
 
-int isCancelled = 0;
 
 /*
  * Write value to path and close file.
@@ -100,7 +97,6 @@ Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, floa
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    isCancelled = 0;
     mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
 
     return Void();
@@ -224,11 +220,9 @@ Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
 }
 
 Return<RequestStatus> BiometricsFingerprint::cancel() {
-    isCancelled = 1;
     mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     mVendorFpService->updateStatus(OP_FINISH_FP_ENROLL);
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
-    set(CANCEL_STATUS_PATH, 1);
 
     return ErrorFilter(mDevice->cancel(mDevice));
 }
@@ -258,11 +252,7 @@ Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid,
 Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId,
         uint32_t gid) {
     set(POWER_STATUS_PATH, 1);
-    set(CANCEL_STATUS_PATH, 0);
-    if (isCancelled)
-        mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
-    else
-        set(AUTH_STATUS_PATH, 1);
+    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
 
     return ErrorFilter(mDevice->authenticate(mDevice, operationId, gid));
