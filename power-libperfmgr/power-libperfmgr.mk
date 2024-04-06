LineageOS/android_hardware_oplus

Add inherit makefiles for libqti-perfd-client/power-libperfmgr
Also, hide libqti-perfd-client behind a soong namespace to avoid warning
on devices that don't use it.

Change-Id: I9d37e9d19b1a6f951a11a9a22eca8c0cf4423dce

power-libperfmgr/power-libperfmgr.mk
@@ -1,0 +1,12 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.oplus-libperfmgr
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel

DerpFest-Devices/hardware_oneplus

power-libperfmgr/power-libperfmgr.mk
@@ -1,0 +1,12 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.oneplus-libperfmgr
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel